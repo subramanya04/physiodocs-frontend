0a32cf5554dc19c90a884262b67196d677ea7597
aamanh30 <aamanh30@gmail.com>
2022-11-06 17:14:23 +0530

    Updated the environment file for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDYv02aLpqUgtJIP9CbT31mOHpu0a77xLM",
-    authDomain: "cdk-admin.firebaseapp.com",
-    databaseURL: "https://cdk-admin.firebaseio.com",
-    projectId: "cdk-admin",
-    storageBucket: "cdk-admin.appspot.com",
-    messagingSenderId: "422055031001",
-    appId: "1:422055031001:web:261c3f03c6dbe830df8ec8",
-    measurementId: "G-NYGXB63DCC"
+    apiKey: 'AIzaSyDYv02aLpqUgtJIP9CbT31mOHpu0a77xLM',
+    authDomain: 'cdk-admin.firebaseapp.com',
+    databaseURL: 'https://cdk-admin.firebaseio.com',
+    projectId: 'cdk-admin',
+    storageBucket: 'cdk-admin.appspot.com',
+    messagingSenderId: '422055031001',
+    appId: '1:422055031001:web:261c3f03c6dbe830df8ec8',
+    measurementId: 'G-NYGXB63DCC'
   },
-  mailApi: '/assets/list.json'
+  mailApi: '/assets/list.json',
+  baseUrl: 'https://physiodocs.net:8000/physiodocs-service'
 };
